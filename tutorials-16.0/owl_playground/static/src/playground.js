498bd16ddf60d6a6b1706ba832c3be41665ec5db
Nico <tmagri.nicholas@gmail.com>
2023-12-07 14:54:33 -0600

    Completed the Generic Card with Slots (Exercise 10)

--- tutorials-16.0/owl_playground/static/src/playground.js
+++ tutorials-16.0/owl_playground/static/src/playground.js
@@ -1,6 +1,7 @@
 /** @odoo-module **/ //Don't forget in all .js files.s
  
 // import { Component, useState } from "@odoo/owl"; // Here we are adding the useState hook (React.js). //Also this line we are importing from Counter.js file. 
+import { Card } from "./card/card";
 import { Counter } from "./counter/counter"; // Here we are importing the Counter.js file to here. 
 // import { Todo } from "./todo/todo"; // Here we are importing the Todo.js file to here to be able to use it.
 import { TodoList } from "./todo_list/todo_list" //Here we are importing everything from the todolist file, so we do not need the todo. 
@@ -9,7 +10,7 @@ import { Component } from "@odoo/owl";
 export class Playground extends Component {
     static template = "owl_playground.playground";
 
-    static components = { Counter, TodoList }; // Here we are calling the counter.js file (that contains all the code below. ) and the TodoList (which will make the code of TodoList run).
+    static components = { Counter, TodoList, Card }; // Here we are calling the counter.js file (that contains all the code below. ) and the TodoList (which will make the code of TodoList run).
 
     // setup(){
     //     this.state = useState({ value: 1 }); //Here we are defining the state value to begin = 1.
